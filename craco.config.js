0c0c5b4bd1ee4205e0fc40f04ffaf540fb9e1db1
Artur <arturpetrosyan199808@gmail.com>
2024-01-24 16:41:48 +0400

    feat(store): HOC
    create the 'withStore' hoc

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+const resolvePath = (p) => path.resolve(__dirname,p)
+
+module.exports = {
+    webpack : {
+        port : '3000',
+        alias : {
+            src : resolvePath('./src'),
+            app : resolvePath('./src/app'),
+            core: resolvePath('./src/core'),
+            pages : resolvePath('./src/pages'),
+            features: resolvePath('./src/features'),
+            widgets: resolvePath('./src/widgets'),
+            entities: resolvePath('./src/entities')
+        }
+    }
+}
