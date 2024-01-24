f88d9f039da6f6bb5fff4827b2d245a0f9ef0441
Artur <arturpetrosyan199808@gmail.com>
2024-01-24 16:39:54 +0400

    feat(App): App
    change the components return

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import AddAllButton from "core/ui/components/styled-components/AddAllButton";
+
+function App() {
+    return (
+        <div className="App">
+            <AddAllButton>
+                Hello
+            </AddAllButton>
+        </div>
+    );
+}
+
+export default App;
