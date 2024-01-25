441522d26a13b369d6c46387e941981431d0ab0c
Artur <arturpetrosyan199808@gmail.com>
2024-01-25 14:51:13 +0400

    feat(App): components use

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,29 @@
 import '../App.css';
-import GlobalStyles from "./styles/globalStyles";
-
+import GlobalStyles from "./assets/styles/globalStyles";
+import Flex from "./assets/styles/Flex";
+import Button from "./assets/styles/Button";
+import Icon from "./assets/styles/Icon";
+import playIcon from "./assets/icons/play.svg"
+import plusIcon from "./assets/icons/plus.svg"
 function App() {
     return (
         <>
             <GlobalStyles/>
             <div className="App">
-
+                <Flex>
+                    <Button>
+                        <Flex justify="space-between" align="center">
+                            <Icon src={playIcon} />
+                            <span>Play All</span>
+                        </Flex>
+                    </Button>
+                    <Button>
+                        <Flex justify="space-between" align="center">
+                            <Icon src={plusIcon} />
+                            <span>Add All</span>
+                        </Flex>
+                    </Button>
+                </Flex>
             </div>
         </>
     );
