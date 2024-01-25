41cfd88685765049614665d4372c06f5e0d4a48e
Artur <arturpetrosyan199808@gmail.com>
2024-01-25 16:52:13 +0400

    feat(Select): styled-component
    create the 'Select' component

---
+++ src/core/ui/components/styled-components/Select.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+const StyledSelect = styled.select`
+    width: 120px;
+    height: 27px;
+`
+const Select = () => {
+    return <StyledSelect />
+};
+
+export default Select;
