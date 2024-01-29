e544dfd69642bff8350eae2a4e22cbc022e5cb04
Artur <arturpetrosyan199808@gmail.com>
2024-01-29 15:06:29 +0400

    fix(Flex): letter

--- src/core/ui/components/styled-components/Flex.js
+++ src/core/ui/components/styled-components/Flex.js
@@ -26,8 +26,8 @@ import styled from "styled-components";
 const StyledFlex = styled.div`
     display: flex;
     flex-direction: ${( {direction} ) => direction || 'row'};
-    align-items: ${( {align} ) => align || 'tretch'};
-    justify-content: ${( {justify} ) => justify || 'tretch'};
+    align-items: ${( {align} ) => align || 'stretch'};
+    justify-content: ${( {justify} ) => justify || 'stretch'};
     gap: 10px;
 `
 
