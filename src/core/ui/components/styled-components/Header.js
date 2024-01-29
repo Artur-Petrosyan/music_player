4fee6881cccb23a9158dfaf5b18b3b840282d648
Artur <arturpetrosyan199808@gmail.com>
2024-01-29 15:07:26 +0400

    feat(header): Js Doc
    add the 'js doc'

--- src/core/ui/components/styled-components/Header.js
+++ src/core/ui/components/styled-components/Header.js
@@ -1,3 +1,30 @@
+/**
+ * A custom header component that uses styled-components to style the header.
+ *
+ * @param {object} props - The component's props.
+ * @param {JSX.Element} props.children - The child elements to render inside the header.
+ * @returns {JSX.Element} The rendered header component.
+ *
+ * @example
+ * import { Header } from './Header';
+ *
+ * const MyComponent = () => {
+ *   return (
+ *     <div>
+ *       <Header>
+ *         <h1>My Header</h1>
+ *         <nav>
+ *           <ul>
+ *             <li><a href="#">Home</a></li>
+ *             <li><a href="#">About</a></li>
+ *             <li><a href="#">Contact</a></li>
+ *           </ul>
+ *         </nav>
+ *       </Header>
+ *     </div>
+ *   );
+ * };
+ */
 import React from 'react';
 import styled from "styled-components";
 
