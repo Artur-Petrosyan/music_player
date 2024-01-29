3f5e88a5095d3e5420614714919040c56ae3231e
Artur <arturpetrosyan199808@gmail.com>
2024-01-29 15:13:30 +0400

    feat(input): styles

--- src/core/ui/components/styled-components/Input.js
+++ src/core/ui/components/styled-components/Input.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import searchIcon from "src/app/assets/icons/search.svg"
 import styled from "styled-components";
+import searchIcon from "src/app/assets/icons/search.svg"
 import Icon from "./Icon";
+
 const StyledInput = styled.input`
     width: 450px;
     border-radius: 30px;
     border: none;
-    padding: 15px 25px;
-    color: rgba(165, 151, 138, 19);
+    padding: 15px 50px;
+    color: black;
     font-size: 22px;
+`
+const StyledInputContainer = styled.div`
+    position: relative;
+
+    & img {
+        position: absolute;
+        width: 30px;
+        height: 30px;
+        left: 13px;
+        top: 13px;
+        cursor: pointer;
+    }
 `
 const Input = () => {
     return (
-        <div>
-            <StyledInput placeholder="Filter" />
-            <Icon src={searchIcon}/>
-        </div>
+        <StyledInputContainer>
+            <StyledInput id="filter__input" name="input" placeholder="Filter" className="filter__input"/>
+            <label htmlFor="filter__input">
+                <Icon src={searchIcon}/>
+            </label>
+        </StyledInputContainer>
     )
 };
 
