6c9d18d72500610bd5ad6626a1bd754c41f7014a
Artur <arturpetrosyan199808@gmail.com>
2024-01-29 15:09:08 +0400

    feat(data): songs data
    create the 'songData'

---
+++ src/app/songs/songsData.js
@@ -0,0 +1,22 @@
+export const songs = [
+    {
+        id : 1 ,
+        songName : "Halo" ,
+        artistName : "BlackBird" ,
+    } ,
+    {
+        id : 2 ,
+        songName : "Halo" ,
+        artistName : "BlackBird" ,
+    } ,
+    {
+        id : 3 ,
+        songName : "Halo" ,
+        artistName : "BlackBird" ,
+    } ,
+    {
+        id : 4 ,
+        songName : "Halo" ,
+        artistName : "BlackBird" ,
+    } ,
+]
