7fc12f7463b50b68aa92c6cca76beb742bd290f0
Artur <arturpetrosyan199808@gmail.com>
2024-01-29 15:13:00 +0400

    feat(components): SongRow
    create the 'SongRow' component and 'SongRow' component style

--- src/core/ui/components/SongRow.jsx
+++ src/core/ui/components/SongRow.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
-const SongRow = () => {
+import styled from "styled-components";
+const StyledSongRow = styled.tr`
+    display: flex;
+    justify-content: space-between;
+    height: inherit;
+    align-items: center;
+    border-bottom: rgba(221, 220, 222, 183) 1px solid;
+    padding: 15px 30px;
+`
+const SongRow = ( {children} ) => {
     return (
-        <div>
-            
-        </div>
+        <StyledSongRow>
+            {children}
+        </StyledSongRow>
     );
 };
 
